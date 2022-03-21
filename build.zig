3d5cf305fa76ff07363a5d08348a1e79998f3ded
maxxnino <34153891+maxxnino@users.noreply.github.com>
2022-03-21 14:35:37 +0900

    replace gyro with submodule

--- build.zig
+++ build.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const pkgs = @import("deps.zig").pkgs;
+
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
@@ -15,7 +15,7 @@ pub fn build(b: *std.build.Builder) void {
     const exe = b.addExecutable("zig-aabb", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
-    pkgs.addAllTo(exe);
+    addPackages(exe);
 
     exe.install();
 
@@ -32,8 +32,15 @@ pub fn build(b: *std.build.Builder) void {
     const tests = b.addTest("src/main.zig");
     tests.setTarget(target);
     tests.setBuildMode(mode);
-    pkgs.addAllTo(tests);
+    addPackages(tests);
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&tests.step);
+}
+
+fn addPackages(lib: *std.build.LibExeObjStep) void {
+    lib.addPackage(.{
+        .name = "zalgebra",
+        .path = .{ .path = "libs/zalgebra/src/main.zig" },
+    });
 }
