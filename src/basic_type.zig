100e69d4e713378f9cec15f2f89b5b939a27b92c
maxxnino <34153891+maxxnino@users.noreply.github.com>
2022-03-21 17:27:27 +0900

    add library class to main.zig

--- src/basic_type.zig
+++ src/basic_type.zig
@@ -2,5 +2,6 @@ const std = @import("std");
 pub const Rect = @import("rect.zig").RectFloat;
 pub const za = @import("zalgebra");
 pub const Vec2 = za.Vec2;
+pub const Vec2u32 = za.GenericVector(2, u32);
 pub const Index = u32;
 pub const node_null = std.math.maxInt(Index);
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,10 @@
 const std = @import("std");
+pub const DynamicTree = @import("DynamicTree.zig");
+pub const Grid = @import("Grid.zig");
+pub const BroadPhase = @import("BroadPhase.zig");
+pub const basic_type = @import("basic_type.zig");
+pub const rect = @import("rect.zig");
+
 test {
     _ = @import("rect.zig");
     _ = @import("DynamicTree.zig");
